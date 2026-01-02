75d009bd801d6d40efe079a46733afa94f93e1f5
Dayen D. Frost <83515342+Dayen-Creation@users.noreply.github.com>
2026-01-03 03:24:57 +0600

    changed menu structure

--- Setup/CreateScriptFromTemplates.cs
+++ Setup/CreateScriptFromTemplates.cs
@@ -22,7 +22,7 @@ namespace DayenCreation.ProjectSetup
 			var PLACEHOLDER_CR = "#COPYRIGHT#";
 			if (templateContent.Contains(PLACEHOLDER_CR))
 			{
-				var copyright = $"///\n/// Copyright (c) 2026 {PlayerSettings.companyName}\n/// All rights reserved.\n///\n/// This file is part of a proprietary project.\n/// Unauthorized copying, modification, or distribution is prohibited.\n///";
+				var copyright = $"/// ------------------------------------------------------------------\n/// Copyright (c) 2026 {PlayerSettings.companyName}\n/// All rights reserved.\n///\n/// This file is part of a proprietary project.\n/// Unauthorized copying, modification, or distribution is prohibited.\n/// ------------------------------------------------------------------";
 				
 				templateContent = templateContent.Replace(PLACEHOLDER_CR, copyright);
 				File.WriteAllText(path, templateContent);
@@ -31,31 +31,31 @@ namespace DayenCreation.ProjectSetup
 			ProjectWindowUtil.CreateScriptAssetFromTemplateFile(path, fileName);
 		}
 
-		[MenuItem("Assets/Create/Code/MonoBehaviour", priority = 1)]
+		[MenuItem("Assets/Code/MonoBehaviour", priority = 100)]
 		public static void CreateMonoBehaviourScript() =>
 			GenerateFile("MonoBehaviour.txt", "NewBehaviour.cs");
 
-		[MenuItem("Assets/Create/Code/Class", priority = 2)]
+		[MenuItem("Assets/Code/Class", priority = 101)]
 		public static void CreateClassScript() =>
 			GenerateFile("Class.txt", "NewClass.cs");
 
-		[MenuItem("Assets/Create/Code/Enum", priority = 3)]
-		public static void CreateEnumScript() =>
-			GenerateFile("Enum.txt", "NewType.cs");
-
-		[MenuItem("Assets/Create/Code/ScriptableObject", priority = 4)]
+		[MenuItem("Assets/Code/ScriptableObject", priority = 102)]
 		public static void CreateScriptableObjectScript() =>
 			GenerateFile("ScriptableObject.txt", "NewSO.cs");
 
-		[MenuItem("Assets/Create/Code/Interface", priority = 5)]
+		[MenuItem("Assets/Code/Enum", priority = 103)]
+		public static void CreateEnumScript() =>
+			GenerateFile("Enum.txt", "NewType.cs");
+
+		[MenuItem("Assets/Code/Interface", priority = 104)]
 		public static void CreateInterfaceScript() =>
 			GenerateFile("Interface.txt", "NewInterface.cs");
 
-		[MenuItem("Assets/Create/Code/Struct", priority = 6)]
+		[MenuItem("Assets/Code/Struct", priority = 105)]
 		public static void CreateStructScript() =>
 			GenerateFile("Struct.txt", "NewStruct.cs");
 
-		[MenuItem("Assets/Create/Code/FiniteState", priority = 7)]
+		[MenuItem("Assets/Code/FiniteState", priority = 106)]
 		public static void CreateFiniteStateScript() =>
 			GenerateFile("FiniteState.txt", "NewState.cs");
 	}
